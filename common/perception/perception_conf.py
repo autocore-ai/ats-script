0045ce19e63e3cd6e31bdb5ea08cbd88982c0c92
kali-autocore <rongjie.duan@autocore.ai>
2020-12-31 15:32:56 +0800

    update perception

--- common/perception/perception_conf.py
+++ common/perception/perception_conf.py
@@ -2,23 +2,23 @@
 from config import TEST_CASE_PATH
 # master info
 PERCEPTION_AUTOWARE4_IP = '127.0.0.1'  # as master
-PERCEPTION_AUTOWARE4_USER = 'adlink'
-PERCEPTION_AUTOWARE4_PWD = 'adlink'
-PERCEPTION_AUTOWARE4_DEVEL = '~/workspace/test_autoware/AutowareArchitectureProposal'
+PERCEPTION_AUTOWARE4_USER = ''
+PERCEPTION_AUTOWARE4_PWD = ''
+PERCEPTION_AUTOWARE4_DEVEL = ''
 
 PERCEPTION_ROS_MASTER_URI = 'http://{}:11311'.format(PERCEPTION_AUTOWARE4_IP)
 
 # perception server info
 PERCEPTION_IP = '127.0.0.1'
-PERCEPTION_USER = 'adlink'
-PERCEPTION_PWD = 'adlink'
+PERCEPTION_USER = ''
+PERCEPTION_PWD = ''
 PERCEPTION_BAG_PATH = '{}/bags/perception'.format(TEST_CASE_PATH)  # record bags, play bags path
 PERCEPTION_BAG_PATH_OPEN = '{}/bags/perception_open'.format(TEST_CASE_PATH)  # record bags, play bags path
 
 PERCEPTION_BAG_REMOTE = False  # record or play bags env, True remote, False local
 PERCEPTION_BAG_REMOTE_IP = '127.0.0.1'
-PERCEPTION_BAG_REMOTE_USER = 'adlink'
-PERCEPTION_BAG_REMOTE_PWD = 'adlink'
+PERCEPTION_BAG_REMOTE_USER = ''
+PERCEPTION_BAG_REMOTE_PWD = ''
 
 # =================================== open source config ===================================
 AUTOWARE_DOCKER_NAME = 'runtime'
