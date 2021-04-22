237310364420b199c228d8b0fd16b79b197f6775
kaka <makaka.0459@gmail.com>
2021-04-22 11:15:14 +0800

    bug:delete files

--- msgs/install/autoware_control_msgs/share/autoware_control_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(autoware_control_msgs_IDL_FILES "msg/EmergencyMode.idl;msg/GateMode.idl;msg/ControlCommand.idl;msg/ControlCommandStamped.idl")
-set(autoware_control_msgs_INTERFACE_FILES "msg/EmergencyMode.msg;msg/GateMode.msg;msg/ControlCommand.msg;msg/ControlCommandStamped.msg")
--- msgs/install/autoware_debug_msgs/share/autoware_debug_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(autoware_debug_msgs_IDL_FILES "msg/BoolStamped.idl;msg/Float32Stamped.idl;msg/Float32MultiArrayStamped.idl;msg/Float64Stamped.idl;msg/Float64MultiArrayStamped.idl;msg/Int32Stamped.idl;msg/Int32MultiArrayStamped.idl;msg/Int64Stamped.idl;msg/Int64MultiArrayStamped.idl;msg/StringStamped.idl")
-set(autoware_debug_msgs_INTERFACE_FILES "msg/BoolStamped.msg;msg/Float32Stamped.msg;msg/Float32MultiArrayStamped.msg;msg/Float64Stamped.msg;msg/Float64MultiArrayStamped.msg;msg/Int32Stamped.msg;msg/Int32MultiArrayStamped.msg;msg/Int64Stamped.msg;msg/Int64MultiArrayStamped.msg;msg/StringStamped.msg")
